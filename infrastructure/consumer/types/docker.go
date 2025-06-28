09a7f9b6d0dc786c4d9c35a667abaa25a41e37bd
Ufi <ydanilau@mail.ru>
2025-06-28 16:58:05 +0900

    [UfiD]: добавил поддержку запуска программ на питоне и с++

--- infrastructure/consumer/types/docker.go
+++ infrastructure/consumer/types/docker.go
@@ -11,10 +11,36 @@ RUN go build -o task task.go
 CMD ["./task"]
 `
 
+const PythonDockerfileContent = `FROM python:alpine
+
+WORKDIR /app
+
+COPY task.py .
+
+CMD ["python", "task.py"]
+`
+
+const CPPDockerfileContent = `FROM alpine:latest
+
+RUN apk add --no-cache g++ musl-dev
+
+WORKDIR /app
+
+COPY task.cpp .
+
+RUN g++ -o task task.cpp
+
+CMD ["./task"]
+`
+
 func GetDockerfileContent(language string) string {
 	switch language {
 	case "go":
 		return GolangDockerfileContent
+	case "python":
+		return PythonDockerfileContent
+	case "cpp":
+		return CPPDockerfileContent
 	default:
 		return ""
 	}
--- infrastructure/consumer/types/types.go
+++ infrastructure/consumer/types/types.go
@@ -4,6 +4,10 @@ func SelectFileExtension(language string) string {
 	switch language {
 	case "go":
 		return "task.go"
+	case "python":
+		return "task.py"
+	case "cpp":
+		return "task.cpp"
 	default:
 		return ""
 	}
